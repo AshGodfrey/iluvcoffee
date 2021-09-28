923aecd7c20c17137510305e1f696e5dbc4a5ec8
inertiawhoosh <victorialgodfrey@gmail.com>
2021-09-28 16:29:37 -0600

    Creating a TypeORM Entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity() //sql table == 'coffee'
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     readonly name: string;
+
+    @Column()
     readonly brand: string;
+
+    @Column('json', { nullable: true })
     readonly flavors: string[];
 }
